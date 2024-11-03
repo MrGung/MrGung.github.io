1c9f67f1c266914907af9c14e201b6927e6aa4d8
Steffen Glückselig <gist@gungfu.de>
2024-11-03 15:33:52 +0100

    tincture

--- cannacalc/cannacalc.cljs
+++ cannacalc/cannacalc.cljs
@@ -22,10 +22,16 @@
 
 ;; ## state
 (defonce app-state (atom {:edible
-                          {:dose-per-portion         10
-                           :number-of-portions       10
-                           :thc-content              10
-                           :efficiency-of-extraction 80}}))
+                          {:dose-per-portion         0
+                           :number-of-portions       0
+                           :thc-content              0
+                           :efficiency-of-extraction 0}
+                          :tincture
+                          {:dose-per-portion         0
+                           :size-of-portion-in-ml    0
+                           :thc-content              0
+                           :efficiency-of-extraction 0
+                           :grams-of-trim            0}}))
 (defn update-state! [app-state nested-id value]
   (swap! app-state assoc-in nested-id value))
 
@@ -54,14 +60,19 @@
         (/ thc-content 100)
         (/ efficiency-of-extraction 100)))))
 
-(defn calculate-tincture-amount-needed [& {:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
+(defn calculate-tincture-amount-needed [& {:keys [dose-per-portion size-of-portion-in-ml thc-content efficiency-of-extraction grams-of-trim] :as values}]
+  ;; a maximum of 1 gram per 1 ml of high-percentage alcohol can be dissolved
   (round-number
     (/
-      (* number-of-portions
-        (/ dose-per-portion 1000))
-      (*
-        (/ thc-content 100)
-        (/ efficiency-of-extraction 100)))))
+      (/
+        (*
+          (* grams-of-trim
+            1000)
+          (*
+            (/ thc-content 100)
+            (/ efficiency-of-extraction 100)))
+        dose-per-portion)
+      size-of-portion-in-ml)))
 
 
 (comment
@@ -69,11 +80,11 @@
   )
 
 
-(defn update-calculation! [{:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
+(defn update-calculation! [values]
   (let [amount (calculate-edible-amount-needed values)
         result-element (.getElementById js/document "edible_result")]
     (set! (.. result-element -innerText) amount)))
-(defn update-calculation-tincture! [{:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
+(defn update-calculation-tincture! [values]
   (let [amount (calculate-tincture-amount-needed values)
         result-element (.getElementById js/document "tincture_result")]
     (set! (.. result-element -innerText) amount)))
