090c7768c723520c89ef8cbcd2347b24fd71354d
Steffen Glückselig <gist@gungfu.de>
2024-11-03 15:07:49 +0100

    Adding tinctures...

--- cannacalc/cannacalc.cljs
+++ cannacalc/cannacalc.cljs
@@ -2,10 +2,15 @@
 ;; ### DOM
 (def log (.-log js/console))
 
+
+(defn get-nested-id [id-value]
+  (let [[category id] (.split id-value "_")]
+    [(keyword category) (keyword id)]))
+
 (defn retrieve-id-and-value [input]
-  (let [id (keyword (.-id input))
+  (let [nested-id (get-nested-id (.-id input))
         value (.-value input)]
-    {:id id :value value}))
+    {:nested-id nested-id :value value}))
 
 
 ;; ### math
@@ -16,24 +21,40 @@
 
 
 ;; ## state
-(defonce app-state (atom {
-                          :dose-per-portion         10
-                          :number-of-portions       10
-                          :thc-content              10
-                          :efficiency-of-extraction 80}))
-(defn update-state! [app-state key value]
-  (swap! app-state assoc key value))
+(defonce app-state (atom {:edible
+                          {:dose-per-portion         10
+                           :number-of-portions       10
+                           :thc-content              10
+                           :efficiency-of-extraction 80}}))
+(defn update-state! [app-state nested-id value]
+  (swap! app-state assoc-in nested-id value))
 
 (declare update-calculation!)
+(declare update-calculation-tincture!)
 
-(add-watch app-state :calculator
+(add-watch app-state :dispatch-calculation
   (fn [key atom old-val new-val]
-    (update-calculation! new-val)))
+    (let [changed? (fn [category]
+                     (not= (category old-val) (category new-val))
+                     )]
+      (cond
+        (changed? :edible) (update-calculation! (:edible new-val))
+        (changed? :tincture) (update-calculation-tincture! (:tincture new-val))
+        ))))
 
 
 ;; ## business - calculating amount
 
-(defn calculate-amount-needed [& {:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
+(defn calculate-edible-amount-needed [& {:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
+  (round-number
+    (/
+      (* number-of-portions
+        (/ dose-per-portion 1000))
+      (*
+        (/ thc-content 100)
+        (/ efficiency-of-extraction 100)))))
+
+(defn calculate-tincture-amount-needed [& {:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
   (round-number
     (/
       (* number-of-portions
@@ -44,38 +65,42 @@
 
 
 (comment
-  (log (calculate-amount-needed :dose-per-portion 10 :number-of-portions 16 :thc-content 20 :efficiency-of-extraction 80))
+  (log (calculate-edible-amount-needed :dose-per-portion 10 :number-of-portions 16 :thc-content 20 :efficiency-of-extraction 80))
   )
 
 
 (defn update-calculation! [{:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
-  (let [amount (calculate-amount-needed values)
-        result-element (.getElementById js/document "result")]
+  (let [amount (calculate-edible-amount-needed values)
+        result-element (.getElementById js/document "edible_result")]
+    (set! (.. result-element -innerText) amount)))
+(defn update-calculation-tincture! [{:keys [dose-per-portion number-of-portions thc-content efficiency-of-extraction] :as values}]
+  (let [amount (calculate-tincture-amount-needed values)
+        result-element (.getElementById js/document "tincture_result")]
     (set! (.. result-element -innerText) amount)))
 
 
 
 ;; ## initialization
 
-(defn register-handler! [inputs input]
+(defn register-handler! [input]
   (.addEventListener input "input"
     (fn []
-      (let [{:keys [id value]} (retrieve-id-and-value input)]
+      (let [{:keys [nested-id value]} (retrieve-id-and-value input)]
         (set! (.-value (.-nextElementSibling input)) value)
-        (update-state! app-state id value)))))
+        (update-state! app-state nested-id value)))))
 
 
 (defn set-initial-values! [inputs]
   (run!
     (fn [input]
-      (let [{:keys [id value]} (retrieve-id-and-value input)]
-        (update-state! app-state id value)
+      (let [{:keys [nested-id value]} (retrieve-id-and-value input)]
+        (update-state! app-state nested-id value)
         (set! (.-value (.-nextElementSibling input)) value)))
     inputs))
 
 (defn init []
   (let [inputs (.getElementsByTagName js/document "input")]
-    (run! #(register-handler! inputs %) inputs)
+    (run! register-handler! inputs)
     (set-initial-values! inputs)))
 
 
